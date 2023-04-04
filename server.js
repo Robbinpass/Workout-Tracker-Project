49e6acb957e852525b1d398214b0576997c239ef
AndrewRotolo <rotolo202@gmail.com>
2023-04-04 14:56:20 -0400

    basic server code and added most of the dependencies we'll need

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const roues = require('./constrollers');
+const helpers = require('./utils/helpers')
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const hbs = exphbs.create({ helpers });
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+});
