a44ab2aae1f8d8ff7a78d4d0ce0ef5fd3e068e2f
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-04-06 17:05:00 -0400

    tried to add exercise but I need to put more work on it

--- db/connection.js
+++ db/connection.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection(
     console.log(`Connected to the PROJECT database.`)
 );
 
-db.query("SELECT * FROM workout_db", (err, result) => {
+db.query("SELECT * FROM people", (err, result) => {
     console.log("hello")
 })
 
@@ -82,26 +82,26 @@ const addMember = () => {
             {
                 type: "input",
                 message: "Please enter you first name?",
-                name: "addMember"
+                name: "First_name"
             },
             {
                 type: "input",
                 message: "Please enter you last name?",
-                name: "addMember"
+                name: "Last_name"
             },
             {
                 type: "input",
                 message: "Please enter you username name?",
-                name: "addMember"
+                name: "Username"
             },
             {
                 type: "input",
                 message: "Please enter you password name?",
-                name: "addMember"
+                name: "Password"
             }
         ]).then(answers => {
             //put your answers in the table in department in the group name
-            db.query(`INSERT INTO people(name)
+            db.query(`INSERT INTO people(First_name, Last_name, Username, Password)
                     VALUES(?)`, answers.addMember, (err, results) => {
                         //show the answer in the department // if there an error console it
                 if (err) {
@@ -119,3 +119,52 @@ const addMember = () => {
         })
         return null
 };
+////////////////////////////////////////
+const addExercise = () => {
+    /////////////////the choices from below put in database from department
+    // const deptChoices = () => db.promise().query(`SELECT * FROM people`)
+    //     .then((rows) => {
+    //         let arrNames = rows[0].map(obj => obj.name);
+    //         console.log("ROW :",rows[0])
+    //         return arrNames
+    //     })
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "which muscle do you want to workout?",
+                name: "Target_Muscles",
+                choices: ['Back','Shoulders', 'Legs', 'Chest', 'Arms', 'N/A']
+            },
+            {
+                type: "input",
+                message: "How much weight do you want to lift?",
+                name: "Lifting_Weight"
+            },
+            {
+                type: "list",
+                message: "How many Reps do you want to do?",
+                name: "Reps",
+            },
+            {
+                type: "list",
+                message: "How many Sets do you want to do?",
+                name: "Sets",
+            }
+        ]).then(answers => {
+            // move the id from department and moves it to department_id
+           // db.promise().query(`SELECT id FROM department WHERE name = ?`, answers.Department)
+                // .then(answer => {
+                //     let mappedId = answer[0].map(obj => obj.id);
+                //     // console.log(mappedId[0])
+                //     return mappedId[0]
+                // })
+        //         .then((mappedId) => {
+        //             //the answers from above gets put into the chart
+        //             db.promise().query(`INSERT INTO roles(title, salary, department_id)
+        //         VALUES(?, ?, ?)`, [answers.Title, answers.Salary, mappedId]);
+
+        //         })
+        })
+        return console.log("added new role!")
+        };
