17f932923a235de3d55b22ef6081492c7bcc61b6
AndrewRotolo <rotolo202@gmail.com>
2023-04-06 18:58:22 -0400

    added login/out functionality using placeholder routes/IDs

---
+++ public/css/js/logout.js
@@ -0,0 +1,16 @@
+const logout = async () => {
+    //"logout" is a placeholder. Replace it if we go with something else
+    const response = await fetch('/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        console.log(response.statusText);
+    }
+};
+
+//"logout" is technically a placeholder, but it's probably what we'll be going with. Still, double check.
+document.querySelector('#logout').addEventListener('click', logout);
