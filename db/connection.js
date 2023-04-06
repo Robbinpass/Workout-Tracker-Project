ded995ecf38026036204d22431ca8a620999b240
HarrisonZussman <115054515+HarrisonZussman@users.noreply.github.com>
2023-04-06 16:26:28 -0400

    let you pick between new member and exercise

--- db/connection.js
+++ db/connection.js
@@ -18,3 +18,34 @@ const db = mysql.createConnection(
 db.query("SELECT * FROM workout_db", (err, result) => {
     console.log("hello")
 })
+
+topices();
+
+function topices() {
+    inquirer.prompt([
+        {
+            //the format will be in list
+            type: "list",
+            //the thing to do
+            message: "select options:",
+            // the name of the chart below
+            name: "gym",
+            choices: [
+                "new member",
+                "exercise"
+            ]
+        }
+
+    ]).then(answers => {
+        switch (answers.company) {
+            case "new member":
+                viewMember();
+                break;
+            case "exercise":
+                viewExercise();
+                break;
+                default:
+                    process.exit();
+            }
+        }).catch(err => console.error(err));
+    }
