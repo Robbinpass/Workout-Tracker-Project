9b4be372e3c211aef36e9fec1218c5c072d92a11
jmcavaddy <jakemcavaddy@gmail.com>
2023-04-10 22:31:59 -0400

    Added homeRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 
+
 const exerciseroutes = require('../controllers/Routes/exerciseroutes');
 const userRoutes = require('./Routes/api/userRoutes')
+const homeRoutes = require('./homeRoutes')
 // const apiRoutes = require('./Routes/api');
 
+router.use('/', homeRoutes)
 router.use('/exercise', exerciseroutes);
 router.use('/user', userRoutes)
+
 // router.use('/api', apiRoutes);
 
 module.exports = router;
