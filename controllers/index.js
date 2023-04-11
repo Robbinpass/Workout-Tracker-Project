d55d86d47c1cca8cd53b753d7baf8e1c60b1f7ce
AndrewRotolo <rotolo202@gmail.com>
2023-04-10 20:09:49 -0400

    login/logout routes should work

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const exerciseroutes = require('js\models\exercise.js');
+const apiRoutes = require('./api');
 
 router.use('/exercise', exerciseroutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
